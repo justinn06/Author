c0b758927b96095070e2f5e9726b697773d9b0d1
justinelecoeur <justine.l2024@isturin.it>
2023-01-24 14:18:48 +0100

    jan 24

--- src/Driver.java
+++ src/Driver.java
@@ -3,14 +3,22 @@ import java.sql.SQLOutput;
 public class Driver {
     public static void main(String[] args) {
 
-        Author author1 = new Author("Justin", "123@gmail.com", 'f');
+        Author author1 = new Author("JK Rowling", "123@gmail.com", 'f');
+        //System.out.println(author1.getName());
+        //System.out.println(author1.getEmail());
+        author1.setEmail("rowling@gmail.com");
         System.out.println(author1.toString());
-        System.out.println(author1.getName());
-        author1.setEmail("isturin@gmail.com");
-        System.out.println(author1.getEmail());
 
-        Book book1 = new Book("Harry Potter", author1, 10.50, 3);
+
+        Author author2 = new Author("Steinbeck", "steinbeck@gmail.com", 'm');
+        System.out.println(author2.toString());
+
+        Book book1 = new Book("Harry Potter", author1, 10.50, 0);
+        book1.setSold(100);
         System.out.println(book1.toString());
+
+        Book book2 = new Book("Of Mice and Men", author2, 9.00, 150);
+        System.out.println(book2.toString());
 
     }
 }
